da9289b08ced30baa60d6a38efeec430a4247933
harikumar V <harikumarv9000@gmail.com>
2024-10-11 11:12:53 +0530

    mongoose db connect and post request basics

--- BASICS/src/app.js
+++ BASICS/src/app.js
@@ -1,9 +1,29 @@
 const express= require("express")
 const connectDB = require("../config/mongodb")
 const app=express()
+const User=require('../model/user')
 
-connectDB()
-
-app.listen(3000,()=>{
-    console.log("server is connected to  3000 port")
+app.post("/signup",async(req,res)=>{
+    const user= new User({
+        name:"hari",
+        email:"hari@gmail,com",
+        password:"asdfhkj34"
+    })
+    await user.save();
+    try{
+        res.send("user registerd successfully")
+    }catch(err){ 
+        // error handling
+        res.status(400).send("Error for saving the data"+err.message)
+    }
 })
+connectDB()
+.then(()=>{
+    console.log("data base connection is established")
+    app.listen(3000,()=>{
+        console.log("server is connected to  3000 portSSS")
+    })
+})
+.catch((err)=>{
+    console.log("data base connection is error")
+});
