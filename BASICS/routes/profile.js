5d337666c716a86a74599092d49520337ed12de9
harikumar V <harikumarv9000@gmail.com>
2024-10-13 12:52:12 +0530

    jwt- jwt-middleeware  error cleared

--- BASICS/routes/profile.js
+++ BASICS/routes/profile.js
@@ -4,26 +4,12 @@ const express=require("express");
 const profileRouter = express.Router();
 const jwt = require ("jsonwebtoken");
 const User=require('../model/user');
+const userAuth = require("../middleware/AuthMiddleware");
 
-profileRouter.get("/profile",async(req,res)=>{
+profileRouter.get("/profile",userAuth, async(req,res)=>{
     try{
-        const cookies= req.cookies
-        const {token}=cookies;
-        if(!token){
-            throw new Error("invalid token");
-        }
-
-        const decodedMessage= await jwt.verify(token, "DEV@Tinder$790")
-  
-        console.log(decodedMessage,"tokenss")
-        const {_id} = decodedMessage;
-        console.log(_id,"iddd")
-        const user = await User.findById(_id)
-        if(!user){
-            throw new Error("User does not exist")
-        }
+        const user= req.user 
         res.send(user)
-    
     }catch(err){
         res.status(400).send('error: '+err.message)
     }
